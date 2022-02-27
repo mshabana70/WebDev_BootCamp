e1ea584f881996174f1be44c714c1a0a48c017e8
Mahmoud Shabana <mshabana5656@gmail.com>
2022-02-27 03:21:38 -0500

    gulp-purgecss installed and reduced style.css size

--- ProjectsForLearning/Sass/CSSLibrary/gulpfile.js
+++ ProjectsForLearning/Sass/CSSLibrary/gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 // Functions 
 
@@ -7,12 +8,14 @@ const sass = require('gulp-sass')(require('sass'))
 function buildStyles() {
     return src('./pixie/**/*.scss')
         .pipe(sass())
+        // purge all css selectors that is not used in our html files
+        .pipe(purgecss({content: ['*.html']})) // comment out for all selectors compiled
         .pipe(dest('css'))
 }
 
 // Watcher function to compile on every update and save of scss file
 function watchTask() {
-    watch(['./pixie/**/*.scss'], buildStyles)
+    watch(['./pixie/**/*.scss', '*.html'], buildStyles)
 }
 
 // run functions on export
