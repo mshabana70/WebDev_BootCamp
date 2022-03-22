5cf8cf0845df6a823ba81318717099de8d25b3bb
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-21 20:55:38 -0400

    examples on functions

---
+++ ProjectsForLearning/JavaScript/examples/functions.js
@@ -0,0 +1,12 @@
+// JAVASCRIPT FUNCTIONS
+
+// creating a function
+function myFunc() {
+    var name = "Mahmoud Shabana";
+    console.log(name + " says hi from his function!");
+}
+
+// calling a function
+myFunc();
+
+// Function for robot getting milk
