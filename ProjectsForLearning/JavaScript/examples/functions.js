04acabfc389b5c415b46e3367343bb87826b4d2b
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-23 19:19:55 -0400

    coding challenge 3

--- ProjectsForLearning/JavaScript/examples/functions.js
+++ ProjectsForLearning/JavaScript/examples/functions.js
@@ -56,3 +56,31 @@ function numOfCandy(amountOfMoney) {
 
 var money = 30;
 console.log("You can buy " + numOfCandy(money) + " pieces of candy with $" + money + " dollars!");
+
+// coding exercises
+// calculate your remaining time in days, weeks, months (assuming you live to 90);
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    let days;
+    let weeks;
+    let months;
+    
+    var yearsLeft = 90 - age;
+    months = yearsLeft * 12;
+    weeks = yearsLeft * 52;
+    days = yearsLeft * 365;
+    
+    
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left." );
+    
+/*************Don't change the code below**********/
+}
+
+// testing coding challenge
+lifeInWeeks(23);
+lifeInWeeks(21);
+lifeInWeeks(46);
+lifeInWeeks(55);
