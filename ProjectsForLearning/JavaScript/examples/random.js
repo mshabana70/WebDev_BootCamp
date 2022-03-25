2b25076e90d8d2dbfa6180b4587eb45e464481f9
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-24 21:32:38 -0400

    simulate dice roll

--- ProjectsForLearning/JavaScript/examples/random.js
+++ ProjectsForLearning/JavaScript/examples/random.js
@@ -4,3 +4,10 @@
 const n = Math.random();
 console.log(n); // decimal b/w 0 - 1
 // output is different everytime we run the script
+
+// simulate a dice roll
+
+let randNum = Math.random();
+randNum *= 6; // six sided die
+let dice = Math.floor(randNum) + 1; // we want values 1 - 6, not 0 - 5
+console.log("Dice was rolled ... your result is: " + dice);
