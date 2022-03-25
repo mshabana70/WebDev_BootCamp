7909241dbf646c5e35daddf00f54f9359cd83f26
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-25 19:36:26 -0400

    leap year challenge and flowchart

--- ProjectsForLearning/JavaScript/examples/conditional.js
+++ ProjectsForLearning/JavaScript/examples/conditional.js
@@ -1,6 +1,7 @@
 // JAVASCRIPT CONDITIONALS
 
 // Instead of using the love calculator, lets run conditions in some functions
+console.log("/************CONDITIONAL EXAMPLES************/\n")
 var numToGuess;
 
 function setNumToGuess(number) {
@@ -31,6 +32,7 @@ numberGuesser(27);
 
 // CODING CHALLENGE: BMI
 // Code a BMI Calculator using conditionals
+console.log("\n/************BMI CHALLENGE************/\n")
 function bmiCalculator (weight, height) {
     let interpretation = "";
     var bmi = (weight / (height * height));
@@ -48,3 +50,35 @@ function bmiCalculator (weight, height) {
 }
 
 console.log(bmiCalculator(64, 1.8));
+
+// CODING CHALLENGE: LEAP YEAR
+// Code a Leap Year calculator that takes a year and tells you if its a leap year or not
+console.log("\n/************LEAP YEAR CHALLENGE************/\n")
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+    
+    //Write your code here.    
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                console.log(year + " is a leap year!");
+            } else {
+                console.log(year + " is not a leap year.");
+            }
+        } else {
+            console.log(year + " is a leap year!");
+        }
+    } else {
+        console.log(year + " is not a leap year.");
+    }
+
+/**************Don't change the code below****************/    
+
+}
+
+isLeap(2000);
+isLeap(2024);
+isLeap(1950);
+isLeap(1872);
+isLeap(1937);
