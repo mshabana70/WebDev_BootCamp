cf58f2a41bbb64baf5e01341aa4ae56ecb49b87f
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-27 16:07:29 -0400

    readline for user input

--- ProjectsForLearning/JavaScript/examples/array.js
+++ ProjectsForLearning/JavaScript/examples/array.js
@@ -24,3 +24,25 @@ const testArray = [1, 2, 3, 4, 5, 6];
 console.log(testArray.includes(2)); // Output: true;
 console.log(testArray.includes(6)); // Output: true;
 console.log(testArray.includes(8)); // Output: false;
+
+// Code Challenge: Used what we learn
+const prompt = require('prompt-sync')(); // this is installed from npm, needed for user input
+
+var guestName = prompt("Please enter you name: ");
+
+if (guestList.includes(guestName)) {
+    console.log("We welcome you, " + guestName + ", as our guest!");
+} else {
+    console.log("Im sorry " + guestName + ", it appears you are not on the guest list.");
+}
+
+// Messing with readline
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("What is your name?", name =>{
+    console.log(`Hey there ${name}!`); // must use `` to use format in js
+    readline.close();
+})
